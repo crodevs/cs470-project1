62f11542166acf127921da800f22356d98184442
Carson Rohan <carson.rohan@gmail.com>
2021-03-16 09:47:18 -0500

    Included comments

--- Networking/UDPServer.java
+++ Networking/UDPServer.java
@@ -10,10 +10,12 @@ public class UDPServer
 {
     DatagramSocket socket = null;
 
-    public UDPServer() 
-    {
+    public UDPServer() { }
 
-    }
+    /**
+     * This method listens on port 25565 for incoming packets, then
+     * sends an acknowledgement when it receives a packet
+     */
     public void createAndListenSocket() 
     {
         try 
@@ -23,36 +25,42 @@ public class UDPServer
 
             while (true) 
             {
-                DatagramPacket incomingPacket = new DatagramPacket(incomingData, 
-                		incomingData.length);
+                // set packet length, receive packet, get data from packet and IP/port
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
                 socket.receive(incomingPacket);
                 String message = new String(incomingPacket.getData());
                 InetAddress IPAddress = incomingPacket.getAddress();
                 int port = incomingPacket.getPort();
-                
+
+                // output for server user
                 System.out.println("Received message from client: " + message);
                 System.out.println("Client IP:"+IPAddress.getHostAddress());
                 System.out.println("Client port:"+port);
-                
+
+                // acknowledgment
                 String reply = "Thank you for the message";
                 byte[] data = reply.getBytes();
-                
-                DatagramPacket replyPacket =
-                        new DatagramPacket(data, data.length, IPAddress, port);
-                
+
+                // create packet for acknowledgement
+                DatagramPacket replyPacket = new DatagramPacket(data, data.length, IPAddress, port);
+
+                // send acknowledgement, close socket
                 socket.send(replyPacket);
                 Thread.sleep(2000);
                 socket.close();
             }
-        } 
+        }
+
         catch (SocketException e) 
         {
             e.printStackTrace();
-        } 
+        }
+
         catch (IOException i) 
         {
             i.printStackTrace();
-        } 
+        }
+
         catch (InterruptedException e) 
         {
             e.printStackTrace();
